8b86b6334f1b0b1c689de0ac88fde48ac7d8918b
Francois LEWE <francois.lewe@gmail.com>
2016-10-25 21:18:14 +0200

    lien listes prestataire : ok pavé catégorie de service dans la page d'accueil: ok

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,28 +20,70 @@ class DefaultController extends Controller
      * @Route("/apropos",name="apropos")
      */
     public function aproposAction(){
-        return $this->render('::apropos.html.twig');
+        return $this->render(':apropos:apropos.html.twig');
     }
 
     /**
      * @Route("/contact",name="contact")
      */
     public function contactAction(){
-        return $this->render('::contact.html.twig');
+        return $this->render(':contact:contact.html.twig');
+    }
+
+
+    /**
+     * @Route("/prestaires/listes",name="prestataires_liste")
+     */
+    public function prestataireListeAction(){
+        $repo = $this->getDoctrine()->getRepository('AppBundle:Prestataire');
+        $prestataires = $repo->findAll();
+        if(!$prestataires){
+            $error = array('msg'=>'aucun prestataire trouvé.');
+            return $this->render('error-404.html.twig',['error'=>$error]);
+        }else{
+            return $this->render(':prestataire:list.html.twig',['prestataires'=>$prestataires]);
+        }
     }
 
 
 
+
+
+    /* ================== ci-dessous les 4 blocs d'infos qui composent la page d'accueil ================== */
+
+    /*
+     *  1. ---------------- le bloc bandeau ou Slider -----------------
+     */
     public function  bandeauAction(){
-        return $this->render(':Blocs:bandeau.html.twig');
+        return $this->render(':blocs_home:bandeau.html.twig');
     }
 
+    /*
+    *  2. ---------------- le bloc Recherche ----------------------------
+    */
     public function rechercheAction(){
-        return $this->render(':Blocs:recherche.html.twig');
+        return $this->render(':blocs_home:recherche.html.twig');
     }
+
+    /*
+    *  3. ---------------- le bloc Prestataires (4 derniers prestataires)--
+    */
     public  function prestatairesAction(){
 
         $prestataires = $this->getDoctrine()->getRepository('AppBundle:Prestataire')->findLastPrestataires();
-        return $this->render(':Blocs:prestataires.html.twig',array('prestaires'=>$prestataires));
+        return $this->render(':blocs_home:prestataires.html.twig',array('prestaires'=>$prestataires));
     }
+
+    /*
+    *  4. ---------------- le bloc service du Mois ou service à la UNE ----------------------------
+    */
+    public function serviceDuMoisAction(){
+
+        $repo = $this->getDoctrine()->getRepository('AppBundle:CategorieDeService');
+        $serviceDuMois = $repo->findOneBy(array('enAvant'=>'1'));
+
+        return $this->render(':blocs_home:service.html.twig',['serviceDuMois'=>$serviceDuMois]);
+
+    }
+    /* ======================================= FIN ============================================================ */
 }
